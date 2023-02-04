215ccbd85d64032c48d5e0961d159a16ceddd088
Michael Bunte <michaelwbunte@gmail.com>
2023-02-03 21:06:46 -0800

    Now writes all top posts to a json

--- readreddit.py
+++ readreddit.py
@@ -1,6 +1,7 @@
 from praw.models import MoreComments
 import argparse
 import re
+import json
 from reddit_credentials import get_reddit_credentials
 
 MAXCHARS = 300
@@ -13,16 +14,30 @@ if __name__ == '__main__':
     reddit = get_reddit_credentials()
     subreddit = reddit.subreddit(args['red'])
 
+    top_posts = {
+        "posts": []
+    }
     for submission in subreddit.hot(limit = int(args['vids'])):
         print(submission.title)
         submission.comments.replace_more(limit=None)
 
         post_length = 0
+        top_comments = []
         for top_level_comment in submission.comments:
             post_length += len(top_level_comment.body)
             print(top_level_comment.body)
-
+            top_comments.append(top_level_comment.body)
             if(post_length >= MAXCHARS):
                 break
 
+        top_posts['posts'].append({
+            "posts": submission.title,
+            "top_comments": top_comments
+        })
+
         print('=' * 50)
+
+    
+    json_object = json.dumps(top_posts, indent=4)
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
